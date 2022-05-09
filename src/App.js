b57af9924ee7668ea889a2643aa6ee7636adf1c1
Rakshith <rakshith6861@gmail.com>
2022-05-09 14:10:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [display, setDisplay] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [inputs, setInputs] = useState([]);
+
+
+  // useEffect
+
+  useEffect(() => {
+   
+       fakestore();
+    
+  },[]);
+
+  // fetch api
+  const fakestore = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+
+    const jsonData = await response.json();
+    // console.log("your data", jsonData);
+    setDisplay(jsonData);
+  };
+
+  // search fuction
+  const inputValue = (e) => {
+    let newInput = e.target.value;
+    setInputs(newInput)
+    setSearch(newInput);
+    const searchFilter = display.filter((searchData) => {
+      return searchData.title.toLowerCase().includes(newInput.toLowerCase());
+    });
+
+    if (searchFilter === 0) {
+      setSearch([]);
+    } else {
+      setSearch(searchFilter);
+    }
+  };
+  console.log("search value", search);
+
+  // return (
+  //   <>
+  //   <div className="input">
+  //     <input onChange={inputValue} type="text" placeholder="Search" />
+
+  //   </div>
+  //   <div className="fetch-data">
+  //    {search.map((values)=>{
+  //     //  console.log('your product name', values.title)
+  //      return(
+  //     <div className="api-data">
+  //      <div className="data">
+  //       <h1  className="heading">{values.title}</h1>
+  //       <img className="imgage" src={values.image} alt="ima" />
+  //       <h3 className="price">${values.price}</h3>
+  //       <p className="desc">{values.description}</p>
+  //      </div><hr />
+  //     </div>
+  //      )
+  //    })}
+  //   </div>
+  //  </>
+  // )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div className="input">
+      <input onChange={inputValue} type="text" placeholder="Search" />
     </div>
+    <div>
+    {inputs.length === 0 ? (
+      <div className="fetch-data">
+         {display.map((values)=>{
+           return(
+             <div api-data>
+               <div className="data">
+                <h1  className="heading">{values.title}</h1>
+                <img className="imgage" src={values.image} alt="ima" />
+                <h3 className="price">${values.price}</h3>
+                <p className="desc">{values.description}</p>
+               </div><hr />
+             </div>
+           )
+         })}
+      </div>
+    ) : (
+      <div className="fetch-data">
+      {search.map((values)=>{
+        return(
+          <div api-data>
+          <div className="data">
+           <h1  className="heading">{values.title}</h1>
+           <img className="imgage" src={values.image} alt="ima" />
+           <h3 className="price">${values.price}</h3>
+           <p className="desc">{values.description}</p>
+          </div><hr />
+        </div>
+        )
+      })
+    }
+     </div> 
+    )
+  }
+    </div>
+  </>   
   );
 }
 
